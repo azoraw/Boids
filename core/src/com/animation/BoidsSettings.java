d2983f7e31ef4eda908d2605279bdeb7cb527e41
azoraw <al.gmail@gmail.com>
2021-01-04 23:29:12 +0100

    add settings validation

--- core/src/com/animation/BoidsSettings.java
+++ core/src/com/animation/BoidsSettings.java
@@ -1,8 +1,8 @@
 package com.animation;
 
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 public class BoidsSettings {
 
     private int numberOfBoids = 1000;
@@ -19,4 +19,73 @@ public class BoidsSettings {
     private int neighbourRadius = 100;
 
     private int circleRadius = 10;
+
+    public void setNumberOfBoids(String numberOfBoidsString) {
+        try {
+            numberOfBoids = Integer.parseInt(numberOfBoidsString);
+        } catch (NumberFormatException ignored) {
+        }
+    }
+
+    public void setMaxSpeed(String maxSpeedString) {
+        try {
+            maxSpeed = Integer.parseInt(maxSpeedString);
+        } catch (NumberFormatException ignored) {
+        }
+    }
+
+    public void setInitSpeed(String initSpeedString) {
+        try {
+            initSpeed = Integer.parseInt(initSpeedString);
+        } catch (NumberFormatException ignored) {
+        }
+    }
+
+    public void setAlignmentForce(String alignmentForceString) {
+        try {
+            alignmentForce = Integer.parseInt(alignmentForceString);
+        } catch (NumberFormatException ignored) {
+        }
+    }
+
+    public void setCohesionForce(String cohesionForceString) {
+        try {
+            cohesionForce = Integer.parseInt(cohesionForceString);
+        } catch (NumberFormatException ignored) {
+        }
+    }
+
+    public void setCollisionRepulsionForce(String collisionRepulsionForceString) {
+        try {
+            collisionRepulsionForce = Integer.parseInt(collisionRepulsionForceString);
+        } catch (NumberFormatException ignored) {
+        }
+    }
+
+    public void setCollidingRadius(String collidingRadiusString) {
+        try {
+            collidingRadius = Integer.parseInt(collidingRadiusString);
+            if (collidingRadius > neighbourRadius) {
+                neighbourRadius = collidingRadius;
+            }
+        } catch (NumberFormatException ignored) {
+        }
+    }
+
+    public void setNeighbourRadius(String neighbourRadiusString) {
+        try {
+            neighbourRadius = Integer.parseInt(neighbourRadiusString);
+            if (neighbourRadius < collidingRadius) {
+                collidingRadius = neighbourRadius;
+            }
+        } catch (NumberFormatException ignored) {
+        }
+    }
+
+    public void setCircleRadius(String circleRadiusString) {
+        try {
+            circleRadius = Integer.parseInt(circleRadiusString);
+        } catch (NumberFormatException ignored) {
+        }
+    }
 }
--- core/src/com/animation/SettingsTable.java
+++ core/src/com/animation/SettingsTable.java
@@ -1,7 +1,10 @@
 package com.animation;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 public class SettingsTable extends Table {
 
@@ -15,90 +18,94 @@ public class SettingsTable extends Table {
 
         Label numberOfBoidsLabel = new Label("number of boids: ", skin);
         TextField numberOfBoidsTextField = new TextField(String.valueOf(boidsSettings.getNumberOfBoids()), skin);
+        Label maxSpeedLabel = new Label("max speed: ", skin);
+        TextField maxSpeedTextField = new TextField(String.valueOf(boidsSettings.getMaxSpeed()), skin);
+        Label initSpeedLabel = new Label("init speed: ", skin);
+        TextField initSpeedTextField = new TextField(String.valueOf(boidsSettings.getInitSpeed()), skin);
+        Label alignmentForceLabel = new Label("alignment force: ", skin);
+        TextField alignmentForceTextField = new TextField(String.valueOf(boidsSettings.getAlignmentForce()), skin);
+        Label cohesionForceLabel = new Label("cohesion force: ", skin);
+        TextField cohesionForceTextField = new TextField(String.valueOf(boidsSettings.getCohesionForce()), skin);
+        Label collisionRepulsionForceLabel = new Label("collision repulsion force: ", skin);
+        TextField collisionRepulsionForceTextField = new TextField(String.valueOf(boidsSettings.getCollisionRepulsionForce()), skin);
+        Label collidingRadiusLabel = new Label("collision radius: ", skin);
+        TextField collidingRadiusTextField = new TextField(String.valueOf(boidsSettings.getCollidingRadius()), skin);
+        Label neighbourRadiusLabel = new Label("neighbour radius: ", skin);
+        TextField neighbourRadiusTextField = new TextField(String.valueOf(boidsSettings.getNeighbourRadius()), skin);
+        Label circleRadiusLabel = new Label("circle radius size: ", skin);
+        TextField circleRadiusTextField = new TextField(String.valueOf(boidsSettings.getCircleRadius()), skin);
+
         numberOfBoidsTextField.addListener(event -> {
-            boidsSettings.setNumberOfBoids(Integer.parseInt(numberOfBoidsTextField.getText()));
+            boidsSettings.setNumberOfBoids(numberOfBoidsTextField.getText());
             return false;
         });
+
+        maxSpeedTextField.addListener(event -> {
+            boidsSettings.setMaxSpeed(maxSpeedTextField.getText());
+            return false;
+        });
+
+        initSpeedTextField.addListener(event -> {
+            boidsSettings.setInitSpeed(initSpeedTextField.getText());
+            return false;
+        });
+
+        alignmentForceTextField.addListener(event -> {
+            boidsSettings.setAlignmentForce(alignmentForceTextField.getText());
+            return false;
+        });
+
+        cohesionForceTextField.addListener(event -> {
+            boidsSettings.setCohesionForce(cohesionForceTextField.getText());
+            return false;
+        });
+
+        collisionRepulsionForceTextField.addListener(event -> {
+            boidsSettings.setCollisionRepulsionForce(collisionRepulsionForceTextField.getText());
+            return false;
+        });
+
+        collidingRadiusTextField.addListener(event -> {
+            boidsSettings.setCollidingRadius(collidingRadiusTextField.getText());
+            neighbourRadiusTextField.setText(String.valueOf(boidsSettings.getNeighbourRadius()));
+            return false;
+        });
+
+        neighbourRadiusTextField.addListener(event -> {
+            boidsSettings.setNeighbourRadius(neighbourRadiusTextField.getText());
+            collidingRadiusTextField.setText(String.valueOf(boidsSettings.getCollidingRadius()));
+            return false;
+        });
+
+        circleRadiusTextField.addListener(event -> {
+            boidsSettings.setCircleRadius(circleRadiusTextField.getText());
+            return false;
+        });
+
         this.add(numberOfBoidsLabel);
         this.add(numberOfBoidsTextField);
         this.row();
-
-        Label maxSpeedLabel = new Label("max speed: ", skin);
-        TextField maxSpeedTextField = new TextField(String.valueOf(boidsSettings.getMaxSpeed()), skin);
-        maxSpeedTextField.addListener(event -> {
-            boidsSettings.setMaxSpeed(Integer.parseInt(maxSpeedTextField.getText()));
-            return false;
-        });
         this.add(maxSpeedLabel);
         this.add(maxSpeedTextField);
         this.row();
-
-        Label initSpeedLabel = new Label("init speed: ", skin);
-        TextField initSpeedTextField = new TextField(String.valueOf(boidsSettings.getInitSpeed()), skin);
-        initSpeedTextField.addListener(event -> {
-            boidsSettings.setInitSpeed(Integer.parseInt(initSpeedTextField.getText()));
-            return false;
-        });
         this.add(initSpeedLabel);
         this.add(initSpeedTextField);
         this.row();
-
-        Label alignmentForceLabel = new Label("alignment force: ", skin);
-        TextField alignmentForceTextField = new TextField(String.valueOf(boidsSettings.getAlignmentForce()), skin);
-        alignmentForceTextField.addListener(event -> {
-            boidsSettings.setAlignmentForce(Integer.parseInt(alignmentForceTextField.getText()));
-            return false;
-        });
         this.add(alignmentForceLabel);
         this.add(alignmentForceTextField);
         this.row();
-
-        Label cohesionForceLabel = new Label("cohesion force: ", skin);
-        TextField cohesionForceTextField = new TextField(String.valueOf(boidsSettings.getCohesionForce()), skin);
-        cohesionForceTextField.addListener(event -> {
-            boidsSettings.setCohesionForce(Integer.parseInt(cohesionForceTextField.getText()));
-            return false;
-        });
         this.add(cohesionForceLabel);
         this.add(cohesionForceTextField);
         this.row();
-
-        Label collisionRepulsionForceLabel = new Label("collision repulsion force: ", skin);
-        TextField collisionRepulsionForceTextField = new TextField(String.valueOf(boidsSettings.getCollisionRepulsionForce()), skin);
-        collisionRepulsionForceTextField.addListener(event -> {
-            boidsSettings.setCollisionRepulsionForce(Integer.parseInt(collisionRepulsionForceTextField.getText()));
-            return false;
-        });
         this.add(collisionRepulsionForceLabel);
         this.add(collisionRepulsionForceTextField);
         this.row();
-
-        Label collidingRadiusLabel = new Label("collision radius: ", skin);
-        TextField collidingRadiusTextField = new TextField(String.valueOf(boidsSettings.getCollidingRadius()), skin);
-        collidingRadiusTextField.addListener(event -> {
-            boidsSettings.setCollidingRadius(Integer.parseInt(collidingRadiusTextField.getText()));
-            return false;
-        });
         this.add(collidingRadiusLabel);
         this.add(collidingRadiusTextField);
         this.row();
-
-        Label neighbourRadiusLabel = new Label("neighbour radius: ", skin);
-        TextField neighbourRadiusTextField = new TextField(String.valueOf(boidsSettings.getNeighbourRadius()), skin);
-        neighbourRadiusTextField.addListener(event -> {
-            boidsSettings.setNeighbourRadius(Integer.parseInt(neighbourRadiusTextField.getText()));
-            return false;
-        });
         this.add(neighbourRadiusLabel);
         this.add(neighbourRadiusTextField);
         this.row();
-
-        Label circleRadiusLabel = new Label("circle radius size: ", skin);
-        TextField circleRadiusTextField = new TextField(String.valueOf(boidsSettings.getCircleRadius()), skin);
-        circleRadiusTextField.addListener(event -> {
-            boidsSettings.setCircleRadius(Integer.parseInt(circleRadiusTextField.getText()));
-            return false;
-        });
         this.add(circleRadiusLabel);
         this.add(circleRadiusTextField);
         this.row();
